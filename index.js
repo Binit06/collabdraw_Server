9b089757e4662f8f516832f9d10776032e956b7f
Binit <binitlenka@gmail.com>
2024-06-16 23:14:15 +0530

    Removed Socket-IO switched to plainsocket

--- index.js
+++ index.js
@@ -1,52 +1,99 @@
-// This is a deployment to fix previous errors
 const express = require('express');
 const http = require('http');
-const { Server } = require('socket.io');
+const WebSocket = require('ws')
+const { v4 : uuidv4 } = require('uuid')
 const cors = require('cors');
-
 const app = express();
 const server = http.createServer(app);
-const io = require('socket.io')(server, {
-    cors: {
-        origin: "*",
-        methods: ["GET", "POST"]
+const wss = new WebSocket.Server({ server });
+
+const connectedClients = {};
+
+wss.on('connection', function (socket, req) {
+    const urlParts = req.url.split('?');
+    if (urlParts.length < 2) {
+        console.log('Connection Rejected: No Username Provided');
+        socket.close();
+        return;
     }
+    const queryParams = new URLSearchParams(urlParts[1]);
+    const username = queryParams.get('username');
+    console.log(username)
+
+    if (!username) {
+        console.log('Connection Rejected: No Username Provided');
+        socket.close()
+        return;
+    }
+
+    if (connectedClients[username]) {
+        console.log('User Already Exist Please try a different username');
+        socket.close();
+        return;
+    }
+
+    connectedClients[username] = socket;
+
+    console.log(`New Web Socket Connected for Username ${username}`)
+
+    broadcastOnlineUsers();
+
+    socket.on('message', (data) => {
+        try {
+            const parsedData = JSON.parse(data);
+
+            if(parsedData.type === 'canvasImage') {
+                Object.keys(connectedClients).forEach((username) => {
+                    const connection = connectedClients[username];
+                    const message = JSON.stringify(parsedData);
+                    if(connection !== socket && connection.readyState === WebSocket.OPEN) {
+                        connection.send(message)
+                    }
+                    console.log(username)
+                })
+            } else if (parsedData.type === 'mousePoisition') {
+                console.log(`Received Mouse Position for ${connectedClients.get(socket).id}: `, parsedData.packet);
+            } else {
+                console.log(`Received Unrecognized Data :`, parsedData);
+            }
+        } catch (error) {
+            console.error(`Error Parsing Message : `, error);
+        }
+    })
+
+    socket.on('close', (ws) => {
+        delete connectedClients[username];
+        console.log(`Web Socket connections closed for username ${username}`)
+        broadcastOnlineUsers();
+        socket.terminate();
+    });
 });
 
-const connectedClients = new Map();
-
-// Socket.IO connection handling
-io.on('connection', function (socket) {
-    socket.on('mousePosition', (position) => {
-        connectedClients.set(socket.id, position);
-        const positions = Array.from(connectedClients.values());
-        socket.broadcast.emit('mousePositions', positions);
+function broadcastOnlineUsers() {
+    const onlineUsers = Object.keys(connectedClients);
+    const message = JSON.stringify({
+        type: 'onlineUsers',
+        users: onlineUsers
     });
+    console.log(onlineUsers)
 
-    socket.on('canvasImage', (data) => {
-        socket.broadcast.emit('canvasImage', data);
+    onlineUsers.map((value) => {
+        const connection = connectedClients[value];
+        console.log(connection)
+        connection.send(message);
     });
+}
 
-    socket.on('disconnect', () => {
-        connectedClients.delete(socket.id);
-        const positions = Array.from(connectedClients.values());
-        socket.broadcast.emit('mousePositions', positions);
-    });
-});
-
-// Use CORS middleware to allow all requests from all origins
 app.use(cors({
     origin: "*",
     mthods: ["GET", "POST"],
     allowedHeaders: ["content-type"]
 }));
 
-// Express route handling
 app.get('/', (req, res) => {
     res.send('Socket.IO server is running');
 });
 
-// Start the server
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
