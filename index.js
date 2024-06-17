8f4cc39e60925201959c9788c0fded9808dc82d7
Binit <binitlenka@gmail.com>
2024-06-17 13:36:56 +0530

    Improved Broadcasting and connection

--- index.js
+++ index.js
@@ -1,80 +1,89 @@
 const express = require('express');
 const http = require('http');
-const WebSocket = require('ws')
+const {WebSocket, WebSocketServer} = require('ws')
 const { v4 : uuidv4 } = require('uuid')
 const cors = require('cors');
 const app = express();
 const server = http.createServer(app);
-const wss = new WebSocket.Server({ server });
+const wsServer = new WebSocket.Server({ server });
 
-const connectedClients = {};
+//I am using clients dictionary to maintain all the clients
+const clients = {};
+//I am using users dictionary to maintain all the users
+const users = {};
+//The current imageURL is maintained here
+let imageURL = null;
+//User Activity History
+const userActivity = [];
 
-wss.on('connection', function (socket, req) {
-    const urlParts = req.url.split('?');
-    if (urlParts.length < 2) {
-        console.log('Connection Rejected: No Username Provided');
-        socket.close();
-        return;
-    }
-    const queryParams = new URLSearchParams(urlParts[1]);
-    const username = queryParams.get('username');
-    console.log(username)
-
-    if (!username) {
-        console.log('Connection Rejected: No Username Provided');
-        socket.close()
-        return;
-    }
-
-    connectedClients[username] = socket;
-    broadcastOnlineUsers();
-
-    console.log(`New Web Socket Connected for Username ${username}`)
-
-
-    socket.on('message', (data) => {
-        try {
-            const parsedData = JSON.parse(data);
-            broadcastOnlineUsers();
-
-            if(parsedData.type === 'canvasImage') {
-                Object.keys(connectedClients).forEach((username) => {
-                    const connection = connectedClients[username];
-                    const message = JSON.stringify(parsedData);
-                    if(connection !== socket && connection.readyState === WebSocket.OPEN) {
-                        connection.send(message)
-                    }
-                })
-            } else if (parsedData.type === 'mousePoisition') {
-                console.log(`Received Mouse Position for ${connectedClients.get(socket).id}: `, parsedData.packet);
-            } else {
-                console.log(`Received Unrecognized Data :`, parsedData);
-            }
-        } catch (error) {
-            console.error(`Error Parsing Message : `, error);
-        }
-    })
-
-    socket.on('close', (ws) => {
-        delete connectedClients[username];
-        console.log(`Web Socket connections closed for username ${username}`)
-        broadcastOnlineUsers();
-        socket.terminate();
-    });
-});
-
-function broadcastOnlineUsers() {
-    const onlineUsers = Object.keys(connectedClients);
-    const message = JSON.stringify({
-        type: 'onlineUsers',
-        users: onlineUsers
-    });
-
-    onlineUsers.map((value) => {
-        const connection = connectedClients[value];
-        connection.send(message);
-    });
+const typeDef = {
+    USER_EVENT: 'userevent',
+    CANVAS_EVENT: 'canvasChange'
 }
+function broadcastMessage(json) {
+    //We will be sending the current data to all the clients
+    const data = JSON.stringify(json)
+    for (let userId in clients) {
+        let client = clients[userId];
+        if(client.readyState === WebSocket.OPEN) {
+            client.send(data);
+        }
+    }
+}
+
+function handleMessage(message, userId) {
+    const dataFromClient = JSON.parse(message.toString());
+    const json = {type: dataFromClient.type}
+    if(dataFromClient.type === typeDef.USER_EVENT) {
+        if(dataFromClient.username){
+            users[userId] = dataFromClient;
+            userActivity.push(`${dataFromClient.username} joined to edit the canvas`)
+            json.data = {users, userActivity}
+        } else {
+            handleDisconnect(userId)
+        }
+    } else if (dataFromClient.type === typeDef.CANVAS_EVENT){
+        canvasContent = dataFromClient.content;
+        imageURL = canvasContent
+        json.data = {canvasContent, userActivity}
+    }
+
+    broadcastMessage(json)
+}
+
+function handleDisconnect(userId) {
+    console.log(`${userId} disconnected`);
+    const json = {type: typeDef.USER_EVENT};
+    const username = users[userId]?.username || userId;
+    userActivity.push(`${username} left the canvas`);
+    json.data = { users, userActivity }
+    delete clients[userId];
+    delete users[userId];
+    broadcastMessage(json)
+}
+
+wsServer.on('connection', function (connection) {
+    const userId = uuidv4();
+    clients[userId] = connection;
+    if (imageURL) {
+        const json = {type: typeDef.CANVAS_EVENT};
+        json.data = {canvasContent: imageURL, userActivity}
+        connection.send(JSON.stringify(json));
+    }
+    connection.on('message', (message) => {
+        handleMessage(message, userId);
+    });
+    connection.on('close', () => {
+        handleDisconnect(userId);
+    });
+    connection.on('error', () => {
+        handleDisconnect(userId);
+    });
+    console.log(`${userId} connected`);
+    const json = {type: typeDef.USER_EVENT};
+    json.data = {users, userActivity}
+    broadcastMessage(json)
+})
 
 app.use(cors({
     origin: "*",
@@ -88,5 +97,5 @@ app.get('/', (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+    console.log(`Server and WebSocket is running on port ${PORT}`);
 });
